93426a958a4f7533ce32df053361ec94338835ce
Joshua Burkhow <joshua@dataenthusiast.com>
2017-03-03 12:50:24 -0800

    Initial Linear Regression Example

---
+++ Statistical Methods in R/Statistical Methods in R/linear_regression.R
@@ -0,0 +1,33 @@
+################################################################################
+#                                                                              #
+# Purpose:       An example of a linear regression in R                        #
+#                                                                              #
+# Author:        Joshua Burkhow                                                #
+# Contact:       joshua@dataenthusiast.com                                     #
+#                                                                              #
+# Code created:  2017-03-03                                                    #
+# Last updated:  2017-03-03                                                    #
+# Source:        C:/Users/jburkhow/Documents/GitHub/Statistical-Methods-in-R/  #
+#                Statistical Methods in R/Statistical Methods in R             #
+#                                                                              #
+# Comment:                                                                     #
+#                                                                              #
+################################################################################
+
+# Step One: Acquire a dataset and ensure it is clean and ready for modeling
+
+
+
+# Step Two: Build your model
+
+
+
+# Step Three: Analyze the response & performance of model
+
+
+
+
+# Step Four: Publish or Productionalize Model
+
+
+
